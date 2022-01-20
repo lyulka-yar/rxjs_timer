5f40d446a7317fd45b78b7f75d0352d2235906ac
Yaroslav Lyulka <63869333+lyulka-yar@users.noreply.github.com>
2022-01-20 15:22:00 +0200

    fix limited time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import {buffer, filter, fromEvent, interval} from "rxjs";
+import {buffer, filter, fromEvent, interval, takeWhile} from "rxjs";
 import {debounceTime, map, takeUntil} from "rxjs/operators";
 import {useEffect, useRef, useState} from "react";
 import {Buttons, Display} from "./component";
 import {Card, Container} from "@mui/material";
 
 const App = () => {
-
     const start = useRef(null);
     const stop = useRef(null);
     const wait = useRef(null);
@@ -15,79 +14,92 @@ const App = () => {
     const seconds$ = interval(1000);
 
     useEffect(() => {
-        let tmpTime = {h: 0, m: 0, s: 0}
-        const start$ = fromEvent(start.current, 'click')
-        const stop$ = fromEvent(stop.current, 'click');
-        const reset$ = fromEvent(reset.current, 'click');
-        const waitTmp$ = fromEvent(wait.current, 'click');
+        let tmpTime = {h: 0, m: 0, s: 0};
+        const start$ = fromEvent(start.current, "click");
+        const stop$ = fromEvent(stop.current, "click");
+        const reset$ = fromEvent(reset.current, "click");
+        const waitTmp$ = fromEvent(wait.current, "click");
         const buff$ = waitTmp$.pipe(debounceTime(300)); // is this solution is right?? for task requirements
-        const wait$ = waitTmp$.pipe(buffer(buff$), map(queue => queue.length), filter(clicks => clicks === 2));
 
-// can`t resolve problem with button RESET doesn`t reset state value and restarts timer
+        const wait$ = waitTmp$.pipe(
+            buffer(buff$),
+            map((queue) => queue.length),
+            filter((clicks) => clicks === 2)
+        );
+        let status = true;
+        // can`t resolve problem with button RESET doesn`t reset state value and restarts timer
 
         start$.subscribe(() => {
-            seconds$.pipe(takeUntil(start$),takeUntil(reset$), takeUntil(stop$),takeUntil(wait$)).subscribe(() => {
-                setTimeState({...tmpTime})
-                switch (true) {
+            seconds$
+                .pipe(
+                    takeUntil(start$),
+                    takeUntil(reset$),
+                    takeUntil(stop$),
+                    takeUntil(wait$),
+                    takeWhile(()=> status)
+                )
+                .subscribe(() => {
+                    setTimeState({...tmpTime});
 
-                    case tmpTime.h === 10 && tmpTime.m === 0 && tmpTime.s === 0:
+                    switch (true) {
+                        case tmpTime.h === 0 && tmpTime.m === 0 && tmpTime.s === 5:
+                            status = false;
+                            tmpTime = {h: 0, m: 0, s: 0};
+                            setTimeState({h: 0, m: 0, s: 0});
+                            break;
+                        case tmpTime.m === 59:
+                            tmpTime.h++;
+                            tmpTime.m = 0;
+                            break;
+                        case tmpTime.s === 59:
+                            tmpTime.m++;
+                            tmpTime.s = 0;
+                            break;
 
-                        break
-                    case tmpTime.m === 59:
-                        tmpTime.h++
-                        tmpTime.m = 0
-                        break
-                    case tmpTime.s === 59:
-                        tmpTime.m++
-                        tmpTime.s = 0
-                        break
+                        default:
+                            tmpTime.s++;
+                    }
+                });
+        });
 
-                    default:
-                        tmpTime.s++
-                }
-            })
-        })
         stop$.subscribe(() => {
-            tmpTime = {h: 0, m: 0, s: 0}
-            setTimeState({h: 0, m: 0, s: 0})
-        })
-    }, [])
+            tmpTime = {h: 0, m: 0, s: 0};
+            setTimeState({h: 0, m: 0, s: 0});
+        });
 
+        reset$.subscribe(() => {
+            setTimeState({h: 0, m: 0, s: 0});
+        });
+
+    }, []);
 
     return (
-
-        <Container sx={{
-            height: "90vh",
-            width: "65vw",
-            m: "0 auto",
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "center",
-            justifyContent: "center",
-            fontSize: "68px"
-
-        }}>
+        <Container
+            sx={{
+                height: "90vh",
+                width: "100vw",
+                m: "0 auto",
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "center",
+                justifyContent: "center",
+                fontSize: "68px",
+            }}
+        >
             <Card
                 sx={{
                     p: "1.5rem 1rem",
                     m: "3rem 5rem",
                     boxShadow: "0 0 5px #1d1d1d, 0 0 5px #1d1d1d",
-                    bgcolor: "#e7dcdc"
+                    bgcolor: "#e7dcdc",
                 }}
             >
-
                 <Display timeState={timeState}/>
 
-                <Buttons
-                    start={start}
-                    stop={stop}
-                    wait={wait}
-                    reset={reset}
-                />
-
+                <Buttons start={start} stop={stop} wait={wait} reset={reset}/>
             </Card>
-
-        </Container>);
+        </Container>
+    );
 };
 
 export default App;
--- src/component/Buttons.js
+++ src/component/Buttons.js
@@ -1,53 +1,39 @@
-import React from 'react';
-import {Box, Button} from "@mui/material";
+import React from "react";
+import { Box, Button } from "@mui/material";
 
 function Buttons({ start, wait, stop, reset }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-around",
+        p: "1rem  0",
+        m: "0 auto",
+        width: "50%",
+      }}
+    >
+      <Button
+        color={"success"}
+        size={"small"}
+        variant={"contained"}
+        ref={start}
+      >
+        start
+      </Button>
 
-    return (
-        <Box
-            sx={
-                {
-                    display: "flex",
-                    justifyContent: "space-around",
-                    p: "1rem  0"
-                }
-            }
-        >
+      <Button color={"error"} size={"small"} variant={"contained"} ref={stop}>
+        stop
+      </Button>
 
-            <Button
-                color={"success"}
-                size={"small"}
-                variant={"contained"}
-                ref={start}>
-                start
-            </Button>
+      <Button color={"warning"} size={"small"} variant={"contained"} ref={wait}>
+        wait
+      </Button>
 
-            <Button
-                color={"error"}
-                size={"small"}
-                variant={"contained"}
-                ref={stop}>
-                stop
-            </Button>
-
-            <Button
-                color={"warning"}
-                size={"small"}
-                variant={"contained"}
-                ref={wait}>
-                wait
-            </Button>
-
-            <Button
-                color={"error"}
-                size={"small"}
-                variant={"contained"}
-                ref={reset}>
-                reset
-            </Button>
-
-        </Box>
-    );
+      <Button color={"error"} size={"small"} variant={"contained"} ref={reset}>
+        reset
+      </Button>
+    </Box>
+  );
 }
 
-export {Buttons};
+export { Buttons };
